693a99f93f35199a0eb6fc8d79b96b1460668136
Conan Gammel <Cgammel@Conans-MacBook-Pro-2.local>
2016-06-23 15:26:08 -0500

    building out the ui for public and private

--- bluemix-object-storage-sample2Tests/bluemix_object_storage_sample2Tests.swift
+++ bluemix-object-storage-sample2Tests/bluemix_object_storage_sample2Tests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-@testable import bluemix_object_storage_sample2
+//@testable import bluemix_object_storage_sample2
 
 class bluemix_object_storage_sample2Tests: XCTestCase {
     
@@ -19,18 +19,6 @@ class bluemix_object_storage_sample2Tests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }
