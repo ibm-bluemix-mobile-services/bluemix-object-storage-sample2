dae17192d8cd3454527c86b17529c485bfcf5a54
Conan Gammel <Cgammel@Conans-MacBook-Pro-2.local>
2016-06-27 09:15:39 -0500

    Added photo cells to public and private

---
+++ bluemix-object-store-sample2/PrivatePhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  PrivatePhotoCell.swift
+//  bluemix-object-store-sample2
+//
+//  Created by Conan Gammel on 6/27/16.
+//  Copyright © 2016 Conan Gammel. All rights reserved.
+//
+
+import UIKit
+
+class PrivatePhotoCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var privateImageView: UIImageView!
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
---
+++ bluemix-object-store-sample2/PublicPhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCell.swift
+//  bluemix-object-store-sample2
+//
+//  Created by Conan Gammel on 6/27/16.
+//  Copyright © 2016 Conan Gammel. All rights reserved.
+//
+
+import UIKit
+
+class PublicPhotoCell: UITableViewCell {
+ 
+    
+    @IBOutlet weak var publicImageView: UIImageView!
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
