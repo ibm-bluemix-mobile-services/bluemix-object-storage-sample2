cf844f961a5b0317e9b5abb6e370c1d03e165832
Conan Gammel <Cgammel@Conans-MacBook-Pro-2.local>
2016-06-28 14:08:38 -0500

    added http calls to Node server, need live server to test

---
+++ bluemix-object-store-sample2/AppConstants.swift
@@ -0,0 +1,16 @@
+//
+//  AppConstants.swift
+//  bluemix-object-store-sample2
+//
+//  Created by Conan Gammel on 6/28/16.
+//  Copyright © 2016 Conan Gammel. All rights reserved.
+//
+
+import Foundation
+
+public class Consts{
+    public static let ObjectStorageScheme: String = "https"
+    public static let ObjectStorageHost: String = "myOSNodeHost"
+    public static let ObjectStoragePort: String = "443"
+    
+}
--- bluemix-object-store-sample2/PrivateViewController.swift
+++ bluemix-object-store-sample2/PrivateViewController.swift
@@ -13,20 +13,13 @@ import BMSSecurity
 class PrivateViewController: UIViewController, UIImagePickerControllerDelegate , UINavigationControllerDelegate{
     
     @IBOutlet weak var tableView: UITableView!
-    
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     
     let logger = Logger.logger(forName: "PhotoListController")
-    
     let theFuture = NSDate.distantFuture().timeIntervalSince1970
+    let ObjectStorageEndpoint: String = "private"
     
-//    var objectStorage: ObjectStorage?
-//    var objectStorageContainer: ObjectStorageContainer?
-//    var objectList: [ObjectStorageObject]?
-    
-
-    
-    
+    var objectStoreResource: HttpResource?
 
     @IBAction func addPictureFromNewPicture(sender: AnyObject) {
         let imagePicker:UIImagePickerController = UIImagePickerController()
@@ -46,22 +39,17 @@ class PrivateViewController: UIViewController, UIImagePickerControllerDelegate ,
         
     }
     
-//    override func viewWillAppear(animated: Bool) {
-//        
-//        if objectStorage == nil {
-//            self.connect()
-//        }
-//        
-//    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        self.navigationController!.navigationBarHidden = true
-//        self.navigationController!.toolbarHidden = false
-        
-        
         cameraButton.enabled  = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        self.objectStoreResource = HttpResource(schema:Consts.ObjectStorageScheme, host: Consts.ObjectStorageHost, port: Consts.ObjectStoragePort, path: ObjectStorageEndpoint)
+
+        HttpClient.get(resource: self.objectStoreResource!, headers: nil, completionHandler: {(error, status, headers, data) in
+            if error != nil{
+                self.logger.debug("Get public object list threw error: \(error)")
+            }
+            self.displayObjects(data!)
+        })
     }
     
     override func didReceiveMemoryWarning() {
@@ -69,133 +57,32 @@ class PrivateViewController: UIViewController, UIImagePickerControllerDelegate ,
         super.didReceiveMemoryWarning()
     }
     
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        
-//        guard let objectList = objectList else {
-//            
-//            return 0
-//        }
-//        
-//        return objectList.count
-//    }
-    
-    
-//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        
-//        
-//        let cell = tableView.dequeueReusableCellWithIdentifier("PrivatePhotoCell", forIndexPath: indexPath) as! PublicPhotoCell
-//        
-//        guard let objectList = objectList else {
-//            
-//            return cell
-//        }
-//        
-//        let objectStorageObject = objectList[indexPath.row]
-//        
-//        guard let data = objectStorageObject.data else {
-//            
-//            return cell
-//        }
-//        let image = UIImage(data: data)
-//        
-//        cell.objectImage.image = image
-//        
-//        return cell
-//    }
-    
-//    func connect() {
-//        
-//        let defaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
-//        let token = defaults.stringForKey("token");
-//        self.objectStorage = ObjectStorage(projectId: "<projectId");
-//        
-//        if let objectStorage = objectStorage {
-//            
-//            objectStorage.connect(token!, region: ObjectStorage.REGION_DALLAS, completionHandler: {
-//                (error) in
-//                
-//                if let error = error {
-//                    print("connect error :: \(error)")
-//                } else {
-//                    print("connect success")
-//                    self.setContainer()
-//                }
-//                
-//            })
-//        }
-//    }
-    
-//    func setContainer() {
-//        
-//        self.objectStorage!.retrieveContainer(name: "<container>", completionHandler: { (error, container) in
-//            
-//            if let error = error {
-//                self.logger.error("The following error occurred while attempting to objtain the object storage container: \(error)")
-//            } else {
-//                self.objectStorageContainer = container;
-//                self.setObjectsList()
-//            }
-//        })
-//    }
-    
-//    func setObjectsList() {
-//        
-//        guard let objectStorageContainer = objectStorageContainer else {
-//            
-//            return
-//        }
-//        
-//        objectStorageContainer.retrieveObjectsList { (error, objects) in
-//            
-//            if let error = error {
-//                self.logger.error("The following error occurred while retrieving the list of objects from object storage: \(error)")
-//            } else {
-//                self.objectList = objects
-//                
-//                for object in objects! {
-//                    self.loadData(object)
-//                }
-//            }
-//        }
-//    }
-    
-//    func loadData(forObject: ObjectStorageObject) {
-//        
-//        forObject.load(shouldCache: true) { (error, data) in
-//            
-//            if let error = error {
-//                self.logger.error("The following error occurred while loading an object from the object storage service: \(error)")
-//            } else {
-//                self.performSelectorOnMainThread(#selector(PhotoListController.loadView), withObject: nil, waitUntilDone: false)
-//            }
-//        }
-//    }
-    
+    func displayObjects(objects: NSData){
+        
+    }
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController){
         
         picker.dismissViewControllerAnimated(true, completion: nil)
     }
     
-//    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-//        
-//        let image: UIImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-//        
-//        let now = NSDate().timeIntervalSince1970
-//        let imageName = (theFuture - now) * 1000
-//        
-//        objectStorageContainer?.storeObject(name: "\(imageName)", data: UIImageJPEGRepresentation(image, 0.7)!, completionHandler: { (error, object) in
-//            
-//            if let error = error {
-//                self.logger.error("The following error occurred while loading an image into the object storage service: \(error)")
-//            } else {
-//                
-//                self.setObjectsList()
-//            }
-//        })
-//        
-//        picker.dismissViewControllerAnimated(true, completion: nil)
-//    }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        let image: UIImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        let now = NSDate().timeIntervalSince1970
+        let imageName = (theFuture - now) * 1000
+        
+        HttpClient.put(resource: self.objectStoreResource!.resourceByAddingPathComponent(pathComponent: "\(imageName)"), headers: nil, data: UIImageJPEGRepresentation(image, 0.7)!, completionHandler: { (error, status, headers, data) in
+            if let error = error {
+                self.logger.error("The following error occurred while loading an image into the object storage service: \(error)")
+            } else {
+                //refresh images
+            }
+        })
+
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
 
     
     
