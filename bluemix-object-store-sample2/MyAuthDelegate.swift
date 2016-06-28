ddc317c633fe0ffbfcbc6b746a89b7947078b30f
Conan Gammel <Cgammel@Conans-MacBook-Pro-2.local>
2016-06-28 09:02:29 -0500

    added authdelegate

---
+++ bluemix-object-store-sample2/MyAuthDelegate.swift
@@ -0,0 +1,62 @@
+//
+//  MyAuthDelegate.swift
+//  bluemix-object-store-sample2
+//
+//  Created by Conan Gammel on 6/28/16.
+//  Copyright © 2016 Conan Gammel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyAuthDelegate: AuthenticationDelegate {
+    
+    let logger = Logger.logger(forName: "MyAuthDelegate")
+    
+    let loginViewController: LoginViewController
+    var authContext: AuthenticationContext?
+    
+    init(loginViewController: LoginViewController) {
+        
+        self.loginViewController = loginViewController;
+    }
+    
+    func submitCredentials(username:String, password: String){
+        
+        guard let authContext = authContext else {
+            
+            return
+        }
+        let challengeAnswer:[String:AnyObject] = ["username":username, "password":password]
+        authContext.submitAuthenticationChallengeAnswer(challengeAnswer)
+    }
+    
+    func onAuthenticationChallengeReceived(authContext: AuthenticationContext, challenge: AnyObject){
+        
+        logger.debug("received authentication challenge")
+        
+        self.authContext = authContext
+        self.loginViewController.onAuthChallengeReceived(challenge)
+    }
+    
+    func onAuthenticationSuccess(info: AnyObject?) {
+        
+        logger.debug("authentication successed. received info: \(info)")
+        
+        let attributes = info!["attributes"]
+        let token = attributes!!["token"]
+        
+        let defaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(token, forKey: "token")
+        
+        loginViewController.performSelectorOnMainThread(#selector(loginViewController.handleSuccessfulLogin), withObject: nil, waitUntilDone: false)
+    }
+    
+    func onAuthenticationFailure(info: AnyObject?){
+        
+        logger.debug("authenticaton failured. received info: \(info)")
+        
+        loginViewController.performSelectorOnMainThread(#selector(loginViewController.handleUnsuccessfulLogin), withObject: nil, waitUntilDone: false)
+    }
+    
+}
