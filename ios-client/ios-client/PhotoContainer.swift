e89864aba030e01f00cd7427e2286ccc01a1784c
Adnan Baruni <abaruni@us.ibm.com>
2016-07-12 09:51:10 -0500

    license headers

--- ios-client/ios-client/PhotoContainer.swift
+++ ios-client/ios-client/PhotoContainer.swift
@@ -1,10 +1,15 @@
-//
-//  PublicContainerTableViewController.swift
-//  ios-client
-//
-//  Created by adnan on 7/8/16.
-//  Copyright © 2016 bluemix-mobile-experience. All rights reserved.
-//
+/*
+ *     Copyright 2016 IBM Corp.
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
 
 import UIKit
 import BMSCore
@@ -33,7 +38,6 @@ class PhotoContainer: UITableViewController, UIImagePickerControllerDelegate, UI
     
     override func viewDidLoad() {
         self.path = (self.restorationIdentifier == "publicContainer") ? "/public" : "/private"
-        self.tableTitle = (self.restorationIdentifier == "publicContainer") ? "Shared" : "My Album"
         self.tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.backgroundTap(_:)))
 //        self.swipeRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(self.refreshTableView(_:)))
 //        self.swipeRecognizer?.direction = .Down
@@ -52,6 +56,7 @@ class PhotoContainer: UITableViewController, UIImagePickerControllerDelegate, UI
     
     override func viewWillAppear(animated: Bool) {
         self.refreshTableView(nil)
+        self.tableTitle = (self.restorationIdentifier == "publicContainer") ? "Shared" : "My Album"
         self.navigationItem.leftBarButtonItem = (self.restorationIdentifier == "privateContainer") ? self.logoutButton : nil
         self.navigationItem.title = title
         
